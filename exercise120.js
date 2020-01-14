a2c9d973267360f5b780513893c5c76d4e33df40
David-Ioannis Gugea <davidgugea@yahoo.com>
2020-01-14 18:01:21 +0100

    Exercise done.

--- exercise120.js
+++ exercise120.js
@@ -1,9 +1,9 @@
 // 120. Write a JavaScript program to convert a string to snake case.
 
-const toSnake = (string) => 
+const toSnake = (string) => string.match(/([A-Z]{2,}(?=[A-Z][a-z]|\b)|[A-Z][a-z]*|[a-z]*|[0-9]+)/g).filter((value) => Boolean(value)).join("_"); 
 
-// ([A-Z]{2,}(?=([A-Z][a-z])+[0-9]*|\b)|([A-Z]{1}[a-z]*)|[a-z]*)
-console.log(toSnake('camelCase'));
+// ([A-Z]{2,}(?=[A-Z][a-z]|\b)|[A-Z][a-z]*|[a-z]*|[0-9]+)
+console.log(toSnake('camelCase')); 
 console.log(toSnake('some text'));
 console.log(toSnake('some-mixed_string With spaces_underscores-and-hyphens'));
 console.log(toSnake('AllThe-small Things'));
