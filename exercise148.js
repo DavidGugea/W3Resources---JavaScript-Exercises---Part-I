40dda2072b6c56298141a3b1fbd08d8c8637496a
David-Ioannis Gugea <davidgugea@yahoo.com>
2020-01-15 19:04:25 +0100

    Exercise done.

---
+++ exercise148.js
@@ -0,0 +1,18 @@
+// 148. Write a JavaScript program to hash the input string into a whole number.
+
+String.prototype.hashCode = function() {
+    var hash = 0;
+    if (this.length == 0) {
+        return hash;
+    }
+    for (var i = 0; i < this.length; i++) {
+        var char = this.charCodeAt(i);
+        hash = ((hash<<5)-hash)+char;
+        hash = hash & hash; // Convert to 32bit integer
+    }
+    return hash;
+}
+
+const toHash = (string) => string.hashCode()
+
+console.log(toHash('name'));
